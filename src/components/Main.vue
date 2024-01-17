a80ae4277ff785200c40096078e46f1575ace2ce
oscarcharles <oscarcharles92814@outlook.com>
2024-01-17 16:51:24 -0500

    first and final

---
+++ src/components/Main.vue
@@ -0,0 +1,67 @@
+<script setup>
+import {ref} from 'vue'
+import Modal from './Modal.vue'
+
+const isOpen = ref(false);
+function openModal(_event) {
+  isOpen.value = true;
+}
+function onClose(_event) {
+  isOpen.value = false;
+}
+</script>
+
+<template>
+  <div class="relative w-screen h-screen bg-center bg-[url('./assets/background.png')] bg-cover bg-no-repeat">
+    <div class="w-1/3 h-screen bg-[#611818]" />
+    <div
+      id="title"
+      class="absolute bottom-[222px] left-[18%]"
+    >
+      <p class="font-[Didot] text-white  xl:text-[224px] text-[15.5vw] leading-none" v-if="!isOpen">Explore</p>
+      <div class="flex cursor-pointer" @click="openModal">
+        <img src="../assets/Plus.svg" alt="plus" class="md:w-[43px] md:h-[43px] w-[32px] h-[32px]"/>
+        <p
+          class="ml-4 font-[Lato] text-white sm:text-[25px] text-[20px]"
+        >
+          More Details
+        </p>
+      </div>
+    </div>
+    <div
+      id="social-buttons"
+      class="absolute right-[31px] bottom-[39px]"
+    >
+      <a href="#"><img class="md:mb-[20px] mb-[12px] md:w-[63px] md:h-[63px] w-[48px] h-[48px]" src="../assets/Facebook.svg" alt="facebook" /></a>
+      <a href="#"><img class="md:w-[63px] md:h-[63px] w-[48px] h-[48px]" src="../assets/Instagram.svg" alt="instagram" /></a>
+    </div>
+  </div>
+  <transition name="fade">
+    <Modal
+      v-if="isOpen"
+      :onClose="onClose"
+    />
+  </transition>
+  
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: transform 1s ease;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  transform: scale(1);
+  transform-origin: 18% calc(100% - 210px);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: scale(0);
+  transform-origin: 18% calc(100% - 210px);
+}
+
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps(['onClose'])
+</script>
+
+<template>
+  <div class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full sm:max-w-xl max-w-screen-sm max-h-full top-[50%] translate-y-[-50%] lg:left-[140px] sm:left-[70px]">
+      <!-- Modal content -->
+      <div class="relative bg-white p-8">
+        <!-- Modal header -->
+        <div class="flex items-center justify-between p-4 md:p-5">
+          <h3 class="text-[66px] font-[Didot] text-[#611818] font-semibold dark:text-white">
+              Explore
+          </h3>
+          <button type="button" class="absolute right-5 top-5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" @click="onClose">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="#611818" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
+        <!-- Modal body -->
+        <div class="p-4 md:p-5 space-y-4">
+          <p class="text-base leading-relaxed text-[#282828] font-[Lato]">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+          </p>
+        </div>
+        <!-- Modal footer -->
+        <div class="flex items-center p-4 md:p-5">
+          <button type="button" class="font-[Lato] rounded-none px-12 py-2 text-[18px] font-semibold text-white uppercase bg-gradient-to-r from-[#611818] to-[#A36754]">Read More</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
